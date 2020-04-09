61525150607e5e80390d2988d672e533a895c757
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2020-04-09 16:41:55 +0200

    Install script is not using sudo anymore

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
+
 DIR_TOOLCHAIN=/usr/lib/gap_riscv_toolchain
+read -e -p "Enter the path where the compiler will be installed (you may need to execute this script with sudo): " -i "$DIR_TOOLCHAIN" DIR_TOOLCHAIN
+
+echo $DIR_TOOLCHAIN
+
 if [ ! -d "$DIR_TOOLCHAIN" ]; then
-    sudo mkdir $DIR_TOOLCHAIN
-    sudo cp -rf ./* /usr/lib/gap_riscv_toolchain
-    cd /usr/lib/gap_riscv_toolchain/bin
-    for eachFile in *
-    do
-        sudo ln -fs $PWD/$eachFile /usr/bin/$eachFile
-    done
+    mkdir $DIR_TOOLCHAIN
+    cp -rf ./* /usr/lib/gap_riscv_toolchain
 else
     echo "$DIR_TOOLCHAIN already exist!!! Please remove it if you want to update"
 fi
